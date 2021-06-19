65d42fcd59492910245eefa2bfe0ea7f610a2a51
Aarush-Jha <75258277+Aarush-Jha@users.noreply.github.com>
2021-06-19 21:25:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-// define arrays
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
 
-var names = ["Alfred", "Derick", "Matt", "Tom"];
-var x = -1; 
-
-// update
-
-function update() {
-    if (x <= 2) {
-        x++;
-    } else {
-        x=0;
-    }
-    document.getElementById("name").innerHTML = names[x];
-    document.getElementById("image").src = "p" + (x+1) + ".jpeg";
+function drawCirc(color,x,y) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 10;
+    ctx.arc(x,y,50,0, 2 * Math.PI);
+    ctx.stroke();
 }
 
+function drawLogo() {
+    drawCirc("black",500,300);
+    drawCirc("blue",400,300);
+    drawCirc("red",600,300);
+    drawCirc("yellow",450,350);
+    drawCirc("green",550,350)
+}
